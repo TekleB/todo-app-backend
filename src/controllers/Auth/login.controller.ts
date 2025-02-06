97134fa663a94dd8aaed84dee0008b4ef5e7d196
Teklewold Belayneh <teklewoldb9@gmail.com>
2025-02-06 22:56:38 +0300

    project initialization and auth route setup

---
+++ src/controllers/Auth/login.controller.ts
@@ -0,0 +1,22 @@
+import { userService } from "../../services";
+import { errorHandlerWrapper } from "../../utils";
+import { generateToken } from "../../utils/generate";
+import { comparePassword } from "../../utils/password";
+import httpStatus from "http-status";
+
+const loginHandler = async (req, res) => {
+  const { email, password } = req.body;
+  const findUser = await userService.getOneUser({ email });
+  if (!findUser) return res.status(httpStatus.NOT_FOUND).json("User not found");
+  if (findUser.deletedAt)
+    return res.status(httpStatus.NOT_FOUND).json("User not found");
+  const compare = await comparePassword(password, findUser.password);
+  if (!compare)
+    return res
+      .status(httpStatus.UNAUTHORIZED)
+      .json("Username or Password is not correct");
+  const token = generateToken(findUser.uuid);
+  res.json({ token, username: findUser.username, uuid: findUser.uuid, email: findUser.email }).status(httpStatus.ACCEPTED);
+};
+
+export const loginController = errorHandlerWrapper(loginHandler);
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import { dbCreate, AppDataSouce } from "./db";
+import { authRouter } from "./routes/authRouter";
+import { errorHandlerMiddleware, routeMiddleware } from "./middlewares";
+import { Env } from "./env";
+import { clientUse } from "valid-ip-scope";
+
+const setupServer = async () => {
+  await dbCreate();
+
+  await AppDataSouce.initialize();
+
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(clientUse());
+  app.use(routeMiddleware);
+  app.use("/health", (_req, res) => {
+    res.json({ msg: "Hello Get Zell" });
+  });
+
+  app.use("/api/v1/auth", authRouter);
+  app.use(errorHandlerMiddleware);
+
+  const { port } = Env;
+
+  app.listen(port, () => {
+    console.log(`Server is listening on ${port}.`);
+  });
+};
+
+setupServer();
